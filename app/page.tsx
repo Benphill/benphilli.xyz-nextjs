ad9b62fcecd1006f3311c3e1fc83a9e3d484f54d
“Ben <benphillipswaterloo@gmail.com>
2025-10-10 18:00:57 -0400

    add

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+export const metadata = {
+  title: 'Home',
+  description: 'Ben&apos;s Site',
+};
+
+export default function RingOfFire() {
+  return (
+    <div>
+        <body>
+          <a href='/BandWebsite'>Band Website</a>
+        </body>
+    </div>
+  );
+}
