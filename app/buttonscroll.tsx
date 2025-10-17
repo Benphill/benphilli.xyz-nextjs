6a9e019a39b90a8032f3520e1bb4d57fdf3c23b2
Benphill <benphillipswaterloo@gmail.com>
2025-10-17 13:32:07 -0400

    changed everything again aaaah

--- app/buttonscroll.tsx
+++ app/buttonscroll.tsx
@@ -5,24 +5,33 @@ export default function ImageMarquee() {
   // Sample image URLs - replace with your actual images
   const images = [
     "https://max.nekoweb.org/images/button.png",
-    "https://max.nekoweb.org/images/button.png",
-    "https://max.nekoweb.org/images/button.png",
-    "https://max.nekoweb.org/images/button.png",
-    "https://max.nekoweb.org/images/button.png",
-    "https://max.nekoweb.org/images/button.png"
+    "https://cyber.dabamos.de/88x31/bestcanada.gif",
+    "https://cyber.dabamos.de/88x31/cssdif.gif",
+    "https://penguinjaa.com/images/buttons/neopenguinjaa.gif",
+    "https://dimden.dev/services/images/88x31.gif",
+    "https://cyber.dabamos.de/88x31/ezgif2.gif"
   ];
 
   const links = [
     "https://max.nekoweb.org",
-    "https://max.nekoweb.org",
-    "https://max.nekoweb.org",
-    "https://max.nekoweb.org",
-    "https://max.nekoweb.org",
-    "https://max.nekoweb.org",
+    "",
+    "",
+    "https://penguinjaa.com",
+    "https://dimden.dev/",
+    ""
+  ];
+
+  const alts = [
+    "Max's Apartment",
+    "The Best Place is Canada",
+    "CSS is Difficult",
+    "NEOPENGUINJAA",
+    "Dimden.dev",
+    "Swatch Deltarune"
   ];
 
   return (
-    <div className="max-w-full mt-8 pt-1 pb-1 bg-maincol">
+    <div className="max-w-full mt-4 pt-1 pb-1">
       {/* Container that clips overflow */}
       <div className="relative overflow-hidden">
         {/* Scrolling wrapper - duplicated many times for truly infinite scroll */}
@@ -39,7 +48,7 @@ export default function ImageMarquee() {
               >
                 <Image
                   src={src}
-                  alt={`Image ${idx + 1}`}
+                  alt={alts[idx]}
                   width={88}
                   height={31}
                 />
--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Head from 'next/head'
 import Status from './status'
 import VisitorCounter from "./components/VisitorCounter";
 import ImageMarquee from "./buttonscroll";
+import Image from "next/image";
 
 export const metadata = {
   title: 'Ben ~ Jasper',
@@ -12,42 +13,58 @@ export const metadata = {
 
 export default async function Home() {
   return (
-      <div className="w-[100%] text-red-100 font-mono">
+      <div className="w-[100%] text-pink-950 font-mono">
           <Head>
             <meta name="viewport" content="width=device-width, initial-scale=1.0" />
           </Head>
-        <div className="bg-maincol basis-[100%] border-2 border-black h-52">
+        <div className="bg-maincol basis-[100%] rounded-lg border-1 border-black h-52">
         </div>
         <div className="h-165 basis-1/1 lg:flex items-start pb-5 min-w-0 max-w-[100%]">          
-          <div className="basis-2/5 mr-1 mt-1 p-2">
-            <div className="bg-maincol mr-1 mt-1 p-2 border-2 border-black">
-              <h1 className="text-2xl font-bold font-mono text-shadow-[0px_0px_2px_#fff]">~ Status</h1>
+          <div className="basis-3/10 mr-1 mt-3">
+            <div className="bg-maincol mr-1 mt-1 p-2 border-1 rounded-lg ">
+              <div className="flex w-full justify-items-center">
+                <h1 className="text-2xl font-bold font-mono text-shadow-[0px_0px_2px_#500724]">~ Status   </h1>
+                <Image src="https://web.archive.org/web/20060309092017if_/http://www.geocities.com/dazed_mirage/flowerpuffspin.gif" alt="flower spin" width={20} height={20} className="h-[20px] mt-auto mb-auto mr-auto ml-2"/>
+              </div>
               <br /> <Status />
             </div>
-            <div className="bg-maincol mr-1 mt-4 p-2 border-2 border-black">
-              <h1 className="text-2xl font-bold font-mono text-shadow-[0px_0px_2px_#fff]">~ Navigation</h1>
-              <Link href="/blog" className="hover:text-shadow-[0px_0px_2px_#fff]">
+            <div className="bg-maincol mr-1 mt-4 p-2 border-1 rounded-lg border-black">
+              <div className="flex w-full justify-items-center">
+                <h1 className="text-2xl font-bold font-mono text-shadow-[0px_0px_2px_#500724]">~ Navigation</h1>
+                <Image src="https://web.archive.org/web/20060309092017if_/http://www.geocities.com/dazed_mirage/flowerpuffspin.gif" alt="flower spin" width={20} height={20} className="h-[20px] mt-auto mb-auto mr-auto ml-2"/>
+              </div>
+              <Link href="/blog" className="hover:text-shadow-[0px_0px_2px_#500724]">
                 <br /> - blog <br />
               </Link>
-              <Link href="/obsessions" className="hover:text-shadow-[0px_0px_2px_#fff]">
+              <Link href="/obsessions" className="hover:text-shadow-[0px_0px_2px_#500724]">
                  - obsessions <br />
               </Link>
-              <Link href="/guestbook" className="hover:text-shadow-[0px_0px_2px_#fff]">
+              <Link href="/guestbook" className="hover:text-shadow-[0px_0px_2px_#500724]">
                  - guestbook <br />
               </Link>
-              <Link href="/gallery" className="hover:text-shadow-[0px_0px_2px_#fff]">
+              <Link href="/gallery" className="hover:text-shadow-[0px_0px_2px_#500724]">
                  - gallery <br />
               </Link>
             </div>
-            <div className="bg-maincol mr-1 mt-4 p-2 border-2 border-black">
-              <h1 className="text-2xl font-bold font-mono text-shadow-[0px_0px_2px_#fff]">~ Visits</h1>
+            <div className="bg-maincol mr-1 mt-4 p-2 border-1 rounded-lg border-black">
+              <div className="flex w-full justify-items-center">
+                <h1 className="text-2xl font-bold font-mono text-shadow-[0px_0px_2px_#500724]">~ Visits</h1>
+                <Image src="https://web.archive.org/web/20060309092017if_/http://www.geocities.com/dazed_mirage/flowerpuffspin.gif" alt="flower spin" width={20} height={20} className="h-[20px] mt-auto mb-auto mr-auto ml-2"/>
+              </div>
               <VisitorCounter />
             </div>
           </div>
-          <div className="bg-lightcol basis-3/5 max-w-2xl mt-4 border-2 border-black h-135 min-w-0">
-            <h1 className="text-4xl font-bold font-mono text-shadow-[0px_0px_2px_#fff] text-center">~ Welcome ~</h1>
-            <div>
-              <ImageMarquee />
+          <div className="bg-maincol basis-7/10 max-w-5xl ml-3 mt-4 border-1 rounded-lg border-black h-135 min-w-0">
+            <h1 className="text-4xl font-bold font-mono text-shadow-[0px_0px_2px_#500724] text-center mt-4">~ Welcome ~</h1>
+            <ImageMarquee />
+            <div className="p-4">
+              <p className="text-xl">hey, i'm Ben! (he/him) this is my personal website where i might post some stuff idk</p>
+              <br /> 
+              <Image src="https://web.archive.org/web/20091027021325if_/http://ca.geocities.com/artist_etc/Misc-Animations/Animals-Plants/ani_Fish-Outline2-Says-Hi.gif" alt="fish says hi" width={309} height={40} className="m-auto"></Image>
+              <h1 className="text-2xl text-shadow-[0px_0px_2px_#500724]">about me</h1>
+              <br />
+              <p className="text-xl">i'm a student currently pursuing a career in mechanical engineering or possibly mechatronics. i also play the trombone and love video games like celeste, deltarune, and oneshot. i'm a part of the 2702 Rebels FIRST robotics team, where i've grown a love for cad design and metal manufacturing.</p>
+              <a href="/obsessions" className="text-lg underline">Read More</a>
             </div>
           </div>
         </div>
